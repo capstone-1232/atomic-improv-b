950bc46cf46a565eda98fa660859066f400c33ac
Kanav Midha <kanavmiddha0001@gmail.com>
2024-02-18 16:32:57 -0700

    add semantic content for home page

--- footer.php
+++ footer.php
@@ -12,17 +12,34 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
+
+		<?php the_custom_logo(); ?>
+
+		<div>
+			<nav class="footer-navigation">
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav>
+		</div>
+
 		<div class="site-info">
+			<span>Copyright &copy; <?= date('Y');?> Atomic Improv</span>
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'atomic-improv' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'atomic-improv' ), 'WordPress' );
+				// printf( esc_html__( 'Proudly powered by %s', 'atomic-improv' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
+			<!-- <span class="sep"> | </span> -->
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'atomic-improv' ), 'atomic-improv', '<a href="http://underscores.me/">Underscores.me</a>' );
+				// printf( esc_html__( 'Theme: %1$s by %2$s.', 'atomic-improv' ), 'atomic-improv', '<a href="http://underscores.me/">Underscores.me</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
