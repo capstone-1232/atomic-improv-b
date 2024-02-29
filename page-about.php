ef11d7dae8f76e583df2488879cd4fc19bb88462
ytantawy1 <113364714+ytantawy1@users.noreply.github.com>
2024-02-29 14:15:02 -0700

    Semantic HTML added

--- page-about.php
+++ page-about.php
@@ -4,4 +4,68 @@
 
 // @package atomic-improv
 
+get_header();
+
 ?>
+
+<h1>All About Donovan</h1>
+
+<section>
+    <h2>Meet the Visionary</h2>
+    <h3>Who is this great Improv Artist</h3>
+    <img class="donovan-headshot" alt="Image of Donovan">
+    <p>Hello, improv comedy enthusiasts! 
+My name is Donovan, and I’d love to take a moment or two to introduce myself, and show you what improv comedy is all about.</p>
+</div>
+</section>
+
+<section>
+    <h2>The Improv Enthuthiast</h2>
+    <p>To me, Improv isn’t a day-to-day job. Improv is something that I am incredibly passionate about - something that defines me for who I am. I’ve always enjoyed the “in-the-moment” brand of humour, and being a part of the world of improv comedy has been an exciting journey for me that’s landed me some interesting moments, like being a part of The Amazing Race Canada, or even being able to team up with former CTV host, Graham Neil. As the proud owner of Atomic Improv, I’m dedicated to bringing joy, laughter, and unforgettable moments to you and yours, as we make our evenings all about you!</p>
+
+    <div>
+        <h3>Awards and Achievements</h2>
+        <img src="" alt="">
+        <p>Atomic Improv stars three-time world improvisational champion Donovan Workun. Workun is a world renowned improviser and actor who has starred in movies, TV shows and radio programs including seven seasons on the #1 comedy radio show in Canada – CBC’s The Irrelevant Show, and CMT’s reality hit show Ice Racer Showdown.</p>
+
+        <p>His career highlights include:</p>
+
+        <ul>
+  <li>Performed with Mike Myers, Wayne Brady, Colin Mochrie, Dave Thomas, and Joe Flaherty (SCTV), Kevin McDonald (Kids In The Hall) and Tom Green to name just a few.</li>
+  <li>First improv group to headline Montreal’s Just for Laughs and the San Francisco Comedy Festival.</li>
+  <li>Official performer at the 2012 Summer Olympic games.</li>
+  <li>Half an hour TV special on “CBC Comics”.</li>
+  <li>Featured in Amazing Race Canada.</li>
+  <li>First comedy act to be nominated for Entertainer of the year by The Canadian Association of Campus Activities.</li>
+  <li>Hosted the Red Carpet at The Juno Awards.</li>
+  <li>Starred in movies alongside Tim Curry, Demi Moore, Patrick Swayze, Carmen Electra, Tom Green, Donald and Kiefer Sutherland and WWE’s star Steve Goldberg.</li>
+  <li>Multiple winner of Alberta Motion Picture Industry Awards.</li>
+</ul>
+    </div>
+
+    <div>
+        <h3>Beyond the Stage</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+    </div>
+
+    <div>
+        <h3>The Atomic Improv Story</h3>
+        <img src="" alt="">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+    </div>
+
+    <div>
+        <h3>Let's share a laugh</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+    </div>
+</section>
+
+</section>
+
+<?php
+get_footer();
+?>
+
+
--- page-contact-us.php
+++ page-contact-us.php
@@ -4,4 +4,14 @@
 
 // @package atomic-improv
 
+get_header();
+
 ?>
+
+<h1>Contact Donovan</h1>
+
+<section>
+<h2>Hire this amazing Improv Artist</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
+</section>
