9bc8f5874da58363346cce8d2d05438217a14220
ytantawy1 <113364714+ytantawy1@users.noreply.github.com>
2024-03-06 15:22:14 -0700

    about content updated

--- page-contact-us.php
+++ page-contact-us.php
@@ -15,3 +15,36 @@ get_header();
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quisquam eaque quibusdam ipsa deserunt blanditiis, harum totam rerum modi libero esse qui, nesciunt dicta odio eveniet explicabo maiores, porro voluptates?</p>
 </section>
+
+<form action="" method="post">
+        <div style="display: flex;">
+            <div style="flex: 1;">
+            <label for="first_name">First Name:</label><br>
+                <input type="text" id="first_name" name="first_name" style="border-radius: 20px;"><br>
+            </div>
+            <div style="flex: 1; margin-left: 10px;">
+                <label for="last_name">Last Name:</label><br>
+                <input type="text" id="last_name" name="last_name" style="border-radius: 20px;"><br>
+            </div>
+        </div>
+
+        <label for="email">Email:</label><br>
+        <input type="email" id="email" name="email" style="border-radius: 20px;"><br>
+
+        <label for="contact">Contact:</label><br>
+        <input type="text" id="contact" name="contact" style="border-radius: 20px;"><br>
+
+        <label for="date">Date:</label><br>
+        <input type="date" id="date" name="date" style="border-radius: 20px;"><br>
+
+        <label for="message">Add Message:</label><br>
+        <textarea id="message" name="message" style="border-radius: 20px;"></textarea><br>
+
+        <div style="text-align: center; margin-top: 20px; margin-bottom: 20px;">
+            <input type="submit" value="Submit" style="border-radius: 20px; padding: 10px 20px; background-color: #007bff; color: #fff; border: none;">
+        </div>
+    </form>
+
+<?php
+get_footer();
+?>
