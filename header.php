1f03b6f197709f357dee1db803767f159f588606
Kanav Midha <kanavmiddha0001@gmail.com>
2024-03-12 00:03:14 -0600

    add webp images for both home and services page. Improve accessibility scores on light house audits

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'atomic-improv' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary" tabindex="0"><?php esc_html_e( 'Skip to content', 'atomic-improv' ); ?></a>
 
 	<header id="masthead" class="site-header d-flex justify-content-between align-items-center">
 		<div class="site-branding">
--- page-services.php
+++ page-services.php
@@ -15,25 +15,25 @@ get_header();
 <div>
     <section >
         <h2>Corporate Events</h2>
-        <img src="<?= get_template_directory_uri()?>/img/corporate-event.png" alt="Donovan with his partner, Chris, posing while a comedy show.">
+        <img src="<?= get_template_directory_uri()?>/img/corporate-events.webp" alt="Donovan with his partner, Chris, posing while a comedy show.">
         <p>Pioneers in corporate comedy shows, Atomic Improv is always professional,  incorporating the clients needs and tailoring the show to the event.  This duo delivers cutting edge comedy that keeps the crowds roaring.</p>
         <p>The award winning production has performed thousands  of times across North America and Europe to rave reviews and sold out  crowds. Atomic Improv will make your next corporate function a sure fire success!</p>
-        <button>Book Now!</button>
+        <a href="#" aria-label="Book Corporate Event">Book Corporate Event</a>
     </section>
 
     <section >
         <h2>The Power of Yes</h2>
-        <img src="<?= get_template_directory_uri()?>/img/poy.png" alt="Power of yes logo.">
+        <img src="<?= get_template_directory_uri()?>/img/poy.webp" alt="Power of yes logo.">
         <div>
             <h3>What is it?</h3>
-            <img src="<?= get_template_directory_uri() ?>/img/donovan-graham.png" alt="Donovan Workun (left) and Graham Neil (right) in suits posing for a picture.">
+            <img src="<?= get_template_directory_uri() ?>/img/donovan-graham.webp" alt="Donovan Workun (left) and Graham Neil (right) in suits posing for a picture.">
             <p>Power of Yes! is a corporate workshop hosted by Graham Neil and Donovan Workun that teaches you the creative techniques of improv and how to apply them to improve your business, work, and life.</p>
             <span class="divider"></span>
         </div>
 
         <div>
             <h3>Why should you say "Yes" to the Powe of Yes?</h3>
-            <img src="<?= get_template_directory_uri()?>/img/donovan-graham-2.png" alt="Donovan and Graham shaking hands.">
+            <img src="<?= get_template_directory_uri()?>/img/donovan-graham-2.webp" alt="Donovan and Graham shaking hands.">
             <p>Improv training is a proven skill-building resource with many positive applications. In fact, a recent Forbes article reports that more Fortune 500 companies are using improv to inspire creativity, communication, and productivity in their workforce.</p>
             <p>Each Power of Yes! workshop focuses on researched business principles and can be customized to meet your organization's needs. Power of Yes! will enhance your staff's ability to work as a team in an effective manner and confidently communicate with clients and each other.</p>
             <span class="divider"></span>
@@ -55,15 +55,15 @@ get_header();
             </div>
         </div>
 
-        <button>Book Now!</button>
+        <a href="#" aria-label="Book Power of Yes">Book Power of Yes</a>
     </section>
 
     <section >
         <h2>The Funny Santa Guy</h2>
-        <img src="<?= get_template_directory_uri() ?>/img/santa-donovan.png" alt="Donovan Workun dressed as santa claus sitting on a chair.">
+        <img src="<?= get_template_directory_uri() ?>/img/santa-donovan.webp" alt="Donovan Workun dressed as santa claus sitting on a chair.">
         <p>Ho-ho-holy smokes, is that Donovan Workun guest starring as Santa Claus?! You’d better believe it!</p>
         <p>Coming to a location near you this winter, Donovan’s bringing holiday cheer and joy to you and your community. When you call on this jovial Santa for your event, you’re guaranteed Christmas cheer for the whole family. From pet-friendly photo shoots to an overall festive spirit, Donovan never fails to impress and bring good cheer to your community.</p>
-        <button>Book Now!</button>
+        <a href="#" aria-label="Book the Santa Guy">Book the Santa Guy</a>
     </section>
 
 </div>
