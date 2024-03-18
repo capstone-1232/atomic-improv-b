cee723a213b7f27696ac36435a600043c7087bf3
Kanav Midha <kanavmiddha0001@gmail.com>
2024-03-17 23:05:53 -0600

    add sass file to the theme folder and also output it to dir/styles.css. Edit the functions file to read the styles.

--- functions.php
+++ functions.php
@@ -142,6 +142,8 @@ function atomic_improv_scripts() {
 	wp_enqueue_style( 'atomic-improv-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'atomic-improv-style', 'rtl', 'replace' );
 
+	wp_enqueue_style('atomic-improv-style-sass', get_template_directory_uri() . '/dir/styles.css');
+
 	wp_enqueue_script( 'atomic-improv-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
