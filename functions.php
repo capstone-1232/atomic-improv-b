4de59e7deba60f97f853a4686113890f4eb1e647
Kanav Midha <kanavmiddha0001@gmail.com>
2024-03-22 15:52:10 -0600

    add lenis smooth scroll

--- functions.php
+++ functions.php
@@ -143,6 +143,8 @@ function atomic_improv_scripts() {
 	wp_style_add_data( 'atomic-improv-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'atomic-improv-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+
+	wp_enqueue_script('lenis-smooth-scroll', 'https://unpkg.com/@studio-freight/lenis@1.0.42/dist/lenis.min.js', _S_VERSION);
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
