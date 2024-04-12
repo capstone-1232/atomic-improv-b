669a0f68a1b6b608cf6150cf1d45feed71674baf
KArneson1 <karneson1@nait.ca>
2024-04-12 14:50:18 -0600

    New Hero Image
    Added new image of Chris and Donovan

--- page-services.php
+++ page-services.php
@@ -10,6 +10,7 @@ get_header();
 <div class="light-bg">
     <div class="services-hero">
         <h2>What do we do?</h2>
+        <img src="<?= get_template_directory_uri() ?>/img/lg/whatdowedo-hero.webp" alt="Chris and Donovan contemplate their funniest moments to tell you about.">
     </div>
 
     <section class="services-corporate">
